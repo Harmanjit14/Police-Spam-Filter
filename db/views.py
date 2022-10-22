e266bee274efe44e265edb2f29064ea6f167ebf6
Harman <harmanjit140500@gmail.com>
2022-10-22 23:43:10 +0530

    twilio test

--- db/views.py
+++ db/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from twilio.twiml.voice_response import VoiceResponse
 
 # Create your views here.
+
+def voice():
+    """Respond to incoming phone calls with a 'Hello world' message"""
+    # Start our TwiML response
+    resp = VoiceResponse()
+
+    # Read a message aloud to the caller
+    resp.say("hello world!", voice='alice')
+
+    return str(resp)
--- server/urls.py
+++ server/urls.py
@@ -17,6 +17,7 @@ from graphene_django.views import GraphQLView
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
+from db.views import voice
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,8 @@ urlpatterns = [
 
 urlpatterns += [
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+]
+
+urlpatterns+=[
+    path("/voice", voice)
 ]
