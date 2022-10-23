2ccb2d7b4f688a345cb1aa7bf7762ce9e18bfbbf
Harman <harmanjit140500@gmail.com>
2022-10-23 14:50:49 +0530

    recording save func 5

--- db/views.py
+++ db/views.py
@@ -23,7 +23,7 @@ def voice(request):
 @csrf_exempt
 def recording_complete(request):
     response = VoiceResponse()
-    print(request)
+    # print(request)
     # The recording url will return a wav file by default, or an mp3 if you add .mp3
     # recording_url = request.values['RecordingUrl'] + '.mp3'
 
@@ -31,6 +31,6 @@ def recording_complete(request):
 
     # with open('{}/{}'.format("static/recordings/", filename), 'wb') as f:
     #     f.write(requests.get(recording_url).content)
-    # response.hangup()
+    response.hangup()
 
     return HttpResponse("Not for consumer use")
